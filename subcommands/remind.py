ff8a229fdff97e61a2fc45ca0dc602b461fe51ac
Adrian Zhou <adrian.zhou@foxmail.com>
2022-09-17 09:25:47 -0700

    remind should work now

--- subcommands/remind.py
+++ subcommands/remind.py
@@ -10,7 +10,7 @@ job_path = package_path.joinpath('usr', 'job.toml')
 
 
 @click.command(no_args_is_help=True)
-@click.argument('sanity', nargs=1, required=False)
+@click.argument('sanity', nargs=1, type=int, required=False)
 @click.option('-m', '--max', 'max_', type=int, help='设定理智上限')
 def remind(sanity, max_):
     with open(job_path, 'rb') as job_file:
@@ -21,12 +21,17 @@ def remind(sanity, max_):
             click.echo('理智上限必须大于0')
             return
         job['max_sanity'] = max_
+        with open(job_path, 'wb') as job_file:
+            tomli_w.dump(job, job_file)
     if 'max_sanity' not in job:
-        max_ = click.prompt('请设定理智上限: ')
-        remind(sanity, max_)
+        click.echo('请设定理智上限')
+        click.echo('命令格式: prts remind -m 理智上限')
+        return
     else:
         max_ = job['max_sanity']
-    if diff := sanity - max_ > 0:
+    if not sanity:
+        return
+    if (diff := max_ - sanity) > 0:
         after = diff * 6
         if last_jobid := job.get('last_id', None):
             hide = subprocess.getoutput(f'atrm {last_jobid}')
